362992be79c764a8935acde0c97e6d9f66cff7f5
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-06-07 20:25:10 -0700

    Implement the looks-like matcher by wrapping imagecomparison-java.

--- src/imagecomparison/core.clj
+++ src/imagecomparison/core.clj
@@ -1,3 +1,15 @@
 (ns imagecomparison.core)
 
-(defn looks-like [filename] (fn [actual] false))
+(defn- load-image [filename]
+  (try
+    (net.avh4.util.imagerender.ImageRenderer/getImage filename)
+    (catch net.avh4.util.imagerender.UnrenderableException e
+      nil)))
+
+(defn looks-like [imagename]
+  (fn [actual]
+    (let [path "./"
+          filename (str path imagename)
+          reference-image (load-image filename)
+          result (net.avh4.util.imagecomparison.ImageComparison/compare actual reference-image)]
+      (.isEqual result))))
--- test/imagecomparison/core_test.clj
+++ test/imagecomparison/core_test.clj
@@ -3,8 +3,10 @@
   (:use [midje.sweet]))
 
 (facts "about looks-like"
-	(fact "it returns a function that accepts and argument"
+	(fact "it returns a function"
     (looks-like "example.png") => fn?)
   (facts "about invoking the matching function"
     (fact "returns false when the reference file does not exist"
-      ((looks-like "does-not-exist.png") .anything.) => false)))
+      ((looks-like "does-not-exist.png") "example.png") => false)
+    (fact "returns true when the reference file matches the actual image"
+      ((looks-like "example.png") "example.png") => true)))
