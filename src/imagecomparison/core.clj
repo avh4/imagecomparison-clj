dd050c4e6cd02e29b7bdeb42ce7a65e2dd6d1456
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-06-08 13:52:04 -0700

    Provide information about exceptions that happen during the check.

--- src/imagecomparison/core.clj
+++ src/imagecomparison/core.clj
@@ -9,15 +9,20 @@
 
 (defn looks-like [imagename]
   (fn [actual]
-    (let [path "./"
-          filename (str path imagename)
-          actual-filename (str path "actual-" imagename)
-          reference-image (load-image filename)
-          result (net.avh4.util.imagecomparison.ImageComparison/compare actual reference-image)]
-      (if (.isEqual result)
-        true
-        (do
-          (.writeActualImageToFile result actual-filename)
-          (checking/as-data-laden-falsehood
-            {:notes [(.getFailureMessage result)
-                     (str "actual image saved as " actual-filename)]}))))))
+    (try
+      (let [path "./"
+            filename (str path imagename)
+            actual-filename (str path "actual-" imagename)
+            reference-image (load-image filename)
+            result (net.avh4.util.imagecomparison.ImageComparison/compare actual reference-image)]
+        (if (.isEqual result)
+          true
+          (do
+            (.writeActualImageToFile result actual-filename)
+            (checking/as-data-laden-falsehood
+              {:notes [(.getFailureMessage result)
+                       (str "actual image saved as " actual-filename)]}))))
+      (catch Throwable e
+        (.printStackTrace e)
+        (checking/as-data-laden-falsehood
+          {:notes [e]})))))
